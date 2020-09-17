86d0490546d95189a008974f53e1af457ddfdb87
italotorres <italotorres3@gmail.com>
2020-09-17 17:06:40 -0300

    melhorias para o script loteria_maluca.py

--- loteria_maluca.py
+++ loteria_maluca.py
@@ -4,31 +4,30 @@
 # programa versão 0.1
 #Nome: lança sorte em apostas
 #descrição: progama que gera numeros aleatorios
-#para jogos de azar 
+#para jogos de azar
 #Criador Ítalo
-#Data: 02/09/20 
-# Correções: arrumar o caracter de espaço especial "\". e verificar o porque da repetição do 60 
+#Data: 02/09/20
+# Correções: arrumar o caracter de espaço especial "\". e verificar o porque da repetição do 60
 ##################
 
 from random import choice, sample, randrange, randint
 
 lstu = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] # numeros da unidade
-lstd = [0, 1, 2, 3, 4, 5, 60] # numeros da dezena
-c = int(input("Informe quantos numeros deseja apostar(6-7-8-9)")) # controle de quantidade de giros
-
-if c < 6 or c > 9:
-    print("fora da sequencia")
-    
-else:
-    
-    for i in range(1, c+1):
-	    d = choice(lstd)
-	    u = choice(lstu)
-	    if d == 60:
-	    	
-	    	print('Dezena %s \º  Lance: 60'  %i)
-	    elif u == 0 and d == 0:
-	    	r = randrange(0, 60)
-	    	print('Dezena %s \º  Lance: %s '  %(i,r))
-	    else:
-	    	print('Dezena %s \º  Lance: %s%s '  %(i,d,u))
+lstd = [0, 1, 2, 3, 4, 5, 6] # numeros da dezena
+c = 1
+while c != 0:
+  c = int(input("Informe quantos numeros deseja apostar(6-7-8-9) ou digite 0 para sair")) # controle de quantidade de giros
+  if c < 6 or c > 9:
+   print("fora da sequencia")
+  else:
+        for i in range(1, c+1):
+          d = choice(lstd)
+          u = choice(lstu)
+          if d > 6 and u > 0:
+            r = randrange(1, 60)
+            print('Dezena %s º  Lance: %s'  %(i,r))
+          elif u == 0 and d == 0:
+            r = randrange(1, 60)
+            print('Dezena %s º  Lance: %s '  %(i,r))
+          else:
+          	print('%s º Dezena: %s%s '  %(i,d,u))
