edf47ead21cb54e9e61ff5e0260a79c6c6bc0127
italotorres <italotorres3@gmail.com>
2025-03-11 13:33:27 -0300

    atualização de comentarios conforme a aula DAY-02

--- hello.py
+++ hello.py
@@ -1,11 +1,37 @@
 #!/usr/bin/env python3
 # Comentário de linha
+"""Hello World Multi Linguas
 
+Dependend da lingua configurada no ambiente o programam exibe a mensagem 
+correspondente.
 
-print("Hello, World!") # Comentário de final de linha
+Como usar:
+
+Tenha a variável LANG devidamente ex:
+   export LANG=pt_BR
+
+Execução:
+   python3 hello.py
+            ou
+   ./hello.py
 
 """
-Comentario
+__version__ = "0.0.1"
+__author__ = "Italo Torres"
+__license__ = "Unlicense"
+import os
+# Dunder significa "Underline" duplo, no fim e no inicio da variavel
+current_language = os.getenv("LANG", "en_US")[:5]
+# snake case(Todas as letras em minusculo); Pascal case (alguma letra em maiusculo nos textos em menusculo)
+msg = "Hello, World!"
+if current_language  == "pt_BR":
+    msg = "Olá, Mundo"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+
+print(msg) # Comentário de final de linha
+
+"""Comentario
 multi
 linhas
 """
